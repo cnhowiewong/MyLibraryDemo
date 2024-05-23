0dc93e02d4e77eca559a3853dc166fdd6cc2c4bb
wanghouyi <wanghouyi@hupu.com>
2024-05-23 11:25:21 +0800

    feat: v1.0.0

---
+++ MyLibraryDemo/Classes/DemoUtils.swift
@@ -0,0 +1,16 @@
+//
+//  DemoUtils.swift
+//  MyLibraryDemo
+//
+//  Created by 王厚一 on 2024/5/23.
+//
+
+import Foundation
+
+final class DemoUtils: NSObject {
+    override init() {
+        super.init()
+        
+        print("demo utils init success")
+    }
+}
